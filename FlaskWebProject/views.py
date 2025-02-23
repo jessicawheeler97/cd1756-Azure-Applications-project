660a20d9ca892412222aa29265fbff2c698d9847
jessicawheeler97 <wheelerlynnjessica@gmail.com>
2025-02-23 17:21:19 -0600

    update logging ingo

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -90,22 +90,27 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
+        app.logger.warning('/redirect - Login Failed')
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
+        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+            redirect_uri=url_for('authorized', _external=True, _scheme='https')
+        )
         if "error" in result:
+            app.logger.warning('/redirect - Login Failed for MSAL')
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
         user = User.query.filter_by(username="admin").first()
+        app.logger.warning("/redirect - Login succeeded")
         login_user(user)
         _save_cache(cache)
-    return redirect(url_for('home'))
+    return redirect(url_for('home')
 
 @app.route('/logout')
 def logout():
